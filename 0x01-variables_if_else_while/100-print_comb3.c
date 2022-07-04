a28e93c5a28032eab602f3326456541bfaad4e22
SamuelAmiaka <amiakasamuel010@gmail.com>
2022-07-04 02:11:47 -0700

    print all possible combination of two digit numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,25 +6,20 @@
  */
 int main(void)
 {
-	int i;
-       	int j;
+	int i, j;
 
 	for (i = 0; i < 9; i++)
 		{
 			for (j = i + 1; j < 10; j++)
 			{
-
 				putchar((i % 10) + '0');
-				putchar((j % 10) + '0')
-					;
+				putchar((j % 10) + '0');
 				if (i == '8' && j == '9')
-					continue;
-
+				continue;
 				putchar(',');
-				putchar(' ');
-			}
-		}
-			 
+	putchar(' ');
+}
+}		 
 	putchar('\n');
 	return (0);
 }
