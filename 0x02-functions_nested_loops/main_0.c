297f410c2acb9c05e7b19a7562fa5c8bb9722135
SamuelAmiaka <amiakasamuel010@gmail.com>
2022-07-06 03:50:38 -0700

    prints all natural numbers from n to 98

--- 0x02-functions_nested_loops/main_0.c
+++ 0x02-functions_nested_loops/main_0.c
@@ -68,6 +68,20 @@ int main(void)
 	return (0);
 }
 /**
+ * main - prints all natural numbers from n to 98
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	print_to_98(0);
+	print_to_98(98);
+	print_to_98(111);
+	print_to_98(81);
+	print_to_98(-10);
+	return (0);
+}
+/**
  * main - adds two integers and returns the result
  *
  * Return: always 0
