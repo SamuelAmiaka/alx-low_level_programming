3c48228a23b0b7d3f0394ec1ceda1d4522269322
SamuelAmiaka <amiakasamuel010@gmail.com>
2022-07-06 03:25:42 -0700

    adds two integers and prints the result

--- 0x02-functions_nested_loops/main_0.c
+++ 0x02-functions_nested_loops/main_0.c
@@ -68,6 +68,19 @@ int main(void)
 	return (0);
 }
 /**
+ * main - adds two integers and returns the result
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+
+	n = add(89, 9)
+		printf("%d\n", n);
+	return (0);
+}
+/**
  * main - prints every minute of Jack Bauer
  *
  * Return: Always 0
