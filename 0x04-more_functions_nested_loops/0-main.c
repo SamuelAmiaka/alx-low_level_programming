1c3dacdb63fa698258bb8b2c925bed02d3309af2
SamuelAmiaka <amiakasamuel010@gmail.com>
2022-07-08 13:45:27 -0700

    print numbers fro 0 to 9

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -18,6 +18,16 @@ int main(void)
 	return (0);
 }
 /**
+ * main - prints numbers 0 to 9
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	print_numbers();
+	return (0);
+}
+/**
  * main - function that multiplies two integers
  *
  * Return: Always 0
