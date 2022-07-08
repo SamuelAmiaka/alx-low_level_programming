615196f41a4899e03dc809cdf4dcb951efda5137
SamuelAmiaka <amiakasamuel010@gmail.com>
2022-07-08 13:14:39 -0700

    multiplies two integers

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -18,6 +18,17 @@ int main(void)
 	return (0);
 }
 /**
+ * main - function that multiplies two integers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("%d\n", mul(98, 1024));
+	printf("%d\n", mul(-402, 4096));
+	return (0);
+}
+/**
  * main - checks for a digit from 0 through 9
  *
  * Return: Always 0
