ca7c48b125a2d1f92bdf016db03a0542d5473e6e
SamuelAmiaka <amiakasamuel010@gmail.com>
2022-07-12 05:18:34 -0700

    swaps the values of two integers

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -15,3 +15,20 @@ int main(void)
 	printf("n=%d\n", n);
 	return (0);
 }
+/**
+ * swap_int - swaps the value of two integers
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	a = 98;
+	b = 42;
+	printf("a=%d, b=%d\n", a, b);
+	swap_int(&a, &b);
+	printf("a=%d, b=%d\n", a, b);
+	return (0);
+}
