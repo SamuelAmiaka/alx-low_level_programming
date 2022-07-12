4d201006ea7dd0033a7c2286109ea6f1ab4c732c
SamuelAmiaka <amiakasamuel010@gmail.com>
2022-07-12 05:10:08 -0700

    point to an int and update the value

---
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - takes pointer to int and resets it to 98
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+
+	n = 402;
+	printf("n=%d\n", n);
+	reset_to_98(&n);
+	printf("n=%d\n", n);
+	return (0);
+}
