3e3e2f5e9114e0e3cafabb27bf3d1574fa7eec56
SamuelAmiaka <amiakasamuel010@gmail.com>
2022-07-12 08:19:04 -0700

    convert a string to an integer

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -16,6 +16,33 @@ int main(void)
 	return (0);
 }
 /**
+ * _atoi - converts a string to an integer
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int nb;
+
+	nb = _atoi("98");
+	printf("%d\n", nb);
+	nb = _atoi("-402");
+	printf("%d\n", nb);
+	nb = _atoi("          ------++++++-----+++++--98")
+	printf("%d\n", nb);
+	nb = _atoi("214748364");
+	printf("%d\n", nb);
+	nb = _atoi("0");
+	printf("%d\n", nb);
+	nb = _atoi("Suite 402");
+	printf("%d\n", nb);
+	nb = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA");
+	printf("%d\n", nb);
+	nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
+	printf("%d\n", nb);
+	return (0);
+}
+/**
  * swap_int - swaps the value of two integers
  *
  * Return: always 0
