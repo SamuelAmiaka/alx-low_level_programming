0db5ff2dbbd845a5dabb194b5d973e24a594bd68
SamuelAmiaka <amiakasamuel010@gmail.com>
2022-07-12 06:57:46 -0700

    prints n elements of an array of integers

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -60,6 +60,23 @@ int main(void)
 	return (0);
 }
 /**
+ * print_array - prints n elements of an array of integers
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int array[5];
+
+	array[0] = 98;
+	array[1] = 402;
+	array[2] = -198;
+	array[3] = 298;
+	array[4] = -1024;
+	print_array(array, 5);
+	return (0);
+}
+/**
  * puts_half - prints half of a string
  *
  * Return: always 0
