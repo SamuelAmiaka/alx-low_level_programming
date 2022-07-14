c81de5fc183e5786a1c1fbf3a4ada4b69068f004
SamuelAmiaka <amiakasamuel010@gmail.com>
2022-07-14 04:43:19 -0700

    reverses an array of integers

--- 0x06-pointers_arrays_strings/0-main.c
+++ 0x06-pointers_arrays_strings/0-main.c
@@ -20,6 +20,43 @@ int main(void)
 	return (0);
 }
 /**
+ * reverse_array- reverses an array of integers
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: nothing
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (i != 0)
+		{
+			printf(", ");
+		}
+		printf("%d", a[i]);
+		i++;
+	}
+	printf("\n");
+}
+/**
+ * reverse_array - reverses an array of integers
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
+
+	print_array(a, sizeof(a) / sizeof(int));
+	reverse_array(a, sizeof(a) / sizeof(int));
+	print_array(a, sizeof(a) / sizeof(int));
+	return (0);
+}
+/**
  * _strcmp - compares two strings
  *
  * Return: always 0
