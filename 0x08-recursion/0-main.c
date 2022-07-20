862de037ada9fb8321a19c7006e6be74acac476f
SamuelAmiaka <amiakasamuel010@gmail.com>
2022-07-20 05:11:36 -0700

    print a string

---
+++ 0x08-recursion/0-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	_puts_recursion("Puts with recursion");
+	return (0);
+}
